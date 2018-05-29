b281a71eb51f75e813decc8123d8384e527d4835
Sanjay <sanjhapps@gmail.com>
2018-05-29 12:57:32 +0530

    Data Types and Literal Expressions

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,27 @@
 fun main(args: Array<String>){
 
-    println("Welcome to Kotlin Practices")
-    println("The First Argument is ${args[0]}")
+    val myInt = 40
+    val myLong: Long = myInt.toLong()
 
-    //**********************************  String Template   ***********************
+    println("the type of myLong is ${myLong::class.simpleName}")
+
+    /*To get the Package Name */
+    println("the type of myLong is ${myLong::class.qualifiedName}")
+
+
+    val myLong2 = 40.9
+    val myInt2 = myLong2.toInt() /*downcasting*/
+
+    println("The value of myLong2 is $myLong2")
+
+    println("The value of myInt2 is $myInt2")
+
+
+    val myInt3 = 420
+
+    /*coverting to Floting type value*/
+    val myDouble3 = myInt3.toDouble()
+
+    println("The value of myInt3 is $myInt3")
+    println("The value of myDouble3 is $myDouble3")
 }
