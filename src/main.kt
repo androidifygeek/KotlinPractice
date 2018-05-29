201a483fe800e555565a8651d504ce51e69b268e
Sanjay <sanjhapps@gmail.com>
2018-05-29 15:38:55 +0530

    Use Functions from Kotlin.Math

--- src/main.kt
+++ src/main.kt
@@ -1,28 +1,37 @@
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.round
+
 fun main(args: Array<String>){
 
-    var num1 = 15
+    val num1 = 15
     val num2 = 10
 
 
-    val sum = num1 + num2
-    println("The sum is $sum")
-
-    val sum1 = num1.plus(num2)
-    println("The Sum is $sum1")
-
-    val diff = num1 - num2
-    println("The difference is $diff")
-
-    num1++
-    println("num1 = $num1")
+    val sum = num1.plus(num2)
+    println("The diff is $sum")
 
 
-    /*Kotlin Increment*/
-    val num3 = num1.inc()
-    println("The value of num1 is ${num1++}")
-    println("num3 = $num3")
+    val difference = num1.minus(num2)
+    println("The difference is $difference")
 
-    val num4 = num1.inc()
-    println("The value of n1 is ${++num1}")
-    println("num4 = $num4")
+    val product = num1.times(num2)
+    println("Product = $product")
+
+
+    val quotient: Double = num1.toDouble().div(num2)
+    println("Quotient = $quotient")
+
+
+    val remainder = num1.rem(num2)
+    println("Remainder = $remainder")
+
+    val neg = -234.5
+    val absolute = abs(neg)
+    println("Absolute = $absolute")
+    println("Rounded = ${round(absolute)}")
+
+
+    println("Have a piece of $PI")
+
 }
