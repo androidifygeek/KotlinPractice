1402aa1f11cf791f12903130b4775def4ce1541d
Sanjay <sanjhapps@gmail.com>
2018-05-29 14:33:26 +0530

    Do Math with Operators and Functions

--- src/main.kt
+++ src/main.kt
@@ -1,13 +1,28 @@
 fun main(args: Array<String>){
 
-    val num1 = 15
+    var num1 = 15
     val num2 = 10
 
-    val match = (num1 == num2)
-    println("Match = $match")
 
-    val match1 = num1.equals(num2)
-    println("Match1 = $match1")
+    val sum = num1 + num2
+    println("The sum is $sum")
 
-    println("Comparison Result = ${num1.compareTo(num2)}")
+    val sum1 = num1.plus(num2)
+    println("The Sum is $sum1")
+
+    val diff = num1 - num2
+    println("The difference is $diff")
+
+    num1++
+    println("num1 = $num1")
+
+
+    /*Kotlin Increment*/
+    val num3 = num1.inc()
+    println("The value of num1 is ${num1++}")
+    println("num3 = $num3")
+
+    val num4 = num1.inc()
+    println("The value of n1 is ${++num1}")
+    println("num4 = $num4")
 }
