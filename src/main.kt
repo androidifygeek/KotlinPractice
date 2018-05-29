c0f02926c6e49d350ed99932229af2120086071a
Sanjay <sanjhapps@gmail.com>
2018-05-29 11:37:17 +0530

    Command Line Arguments in Kotlin

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,7 @@
 fun main(args: Array<String>){
 
     println("Welcome to Kotlin Practices")
+    println("The First Argument is ${args[0]}")
+
+    //**********************************  String Template   ***********************
 }
