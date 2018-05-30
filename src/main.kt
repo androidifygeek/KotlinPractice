e16e3b67f4b5d0a2f096176a91e4b653b0dcf313
Sanjay <sanjhapps@gmail.com>
2018-05-30 11:33:58 +0530

    Define constant in a companion object

--- src/main.kt
+++ src/main.kt
@@ -1,12 +1,6 @@
-fun main(args: Array<String>){
+import Constants.Companion.RED
 
-    val builder = StringBuilder("पंचतंत्र की कहानियाँ मनोविज्ञान,\n")
-            .append("व्यवहारिकता तथा राजकाज के सिद्धांतों के विषयों को रोचक ढंग से प्रस्तुत करती हैं\n")
-            .append("तथा इन सभी विषयों की सीख देती हैं।")
+fun main(args: Array<String>) {
 
-    /*StringBuilder class in Kotlin is Type Alias*/
-    val result = builder.toString()
-    println(result)
-
-
+    println("The color is $RED")
 }
