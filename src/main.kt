609ed7ec1cd0600f299abde8a965e6e3dafe566e
Sanjay <sanjhapps@gmail.com>
2018-05-30 14:44:28 +0530

    Evaluate Conditions with if and else

--- src/main.kt
+++ src/main.kt
@@ -1,64 +1,49 @@
-import kotlin.reflect.jvm.internal.impl.utils.NumbersKt
+import javafx.beans.binding.When
 
 fun main(args: Array<String>) {
 
-    val num1 = 4.0
-    val num2 = 3.5
+
+    print("Enter a State Abbreviation: ")
+    val state = readLine()
+
+//    if (state == "CA") {
+//        println("The Capital is Sacramento")
+//    } else if (state == "OR") {
+//        println("The Capital is Salem")
+//
+//    }
+
+//    val capital: String?
+//    when(state){
+//        "CA" -> capital = "Sacramento"
+//        "OR" -> capital = "Salem"
+//        else -> capital="unknown"
+//    }
 
 
-    val result = addValues(param1 = num1, param2 = num2)
-    println("The result is $result")
-
-    val Result2 = calcValues(param1 = num1, param2 = num2, op = "-")
-    println("The result 2 is $Result2")
-
-    changeSomething(5.0)
-
-    val sum: Int = addValues(1, 3, 5, 7)
-    println("Sum=$sum")
-}
-
-fun addValues(param1: Double, param2: Double): Double {
-    return param1 + param2
-
-}
+    val capital = when (state) {
+        "CA" -> "Sacramento"
+        "OR" -> "Salem"
+        else -> "unknown"
+    }
+    println("The capital is $capital")
 
 
-fun calcValues(param1: Double, param2: Double, op: String = "+"): Double {
-
-    if (op.equals("+")) {
-        return param1 + param2
-    } else if (op.equals("-")) {
-        return param1 - param2
-    } else {
-        return -1.0
+    when (state) {
+        "CA", "OR", "WA" -> println("WEST COAST")
+        "NH", "VT", "MA" -> println("NEW ENGLAND")
+        else -> println("Somewhere Else")
     }
 
-    /*simplyfy code for calcValues()*/
-    /* return when (op) {
-         "+" -> param1 + param2
-         "_" -> param1 - param2
-         else -> -1.0
-     }*/
+    val theAnswer = 46
+    when (theAnswer) {
+        in 1..39 -> println("Not Yet")
+        in 40..45 -> println("close enough")
+        else -> {
+            println("Not Really!")
+            println("Definitely Not")
+        }
 
-}
-
-fun changeSomething(param: Double) {
-    //param++
-
-    var copy = param
-    copy++
-
-    println("Copy is $copy")
-
-
-}
-
-fun addValues(vararg numbers: Int): Int {
-    var result = 0
-    for (i in numbers) {
-        result += i
     }
-    return result
 
 }
